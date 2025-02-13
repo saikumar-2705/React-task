007f785d513d013c4498cac334a77fa0a4847389
saikumar-2705 <saikumarb2705@gmail.com>
2025-02-13 16:35:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+      import React, { useState } from "react";
+import Form from "./components/Form";
+import CardList from "./components/CardList";
+import Home from "./components/Home";
+import "./App.css";
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [placedCount, setPlacedCount] = useState(0);
+
+  const addCard = (card) => {
+    setCards([...cards, card]);
+    if (card.status === "Placed") setPlacedCount(placedCount + 1);
+  };
+
+  const deleteCard = (id) => {
+    const updatedCards = cards.filter((_, index) => index !== id);
+    setCards(updatedCards);
+
+    if (cards[id].status === "Placed") {
+      setPlacedCount(placedCount - 1);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Home totalCards={cards.length} placed={placedCount} unplaced={cards.length - placedCount} />
+      <Form addCard={addCard} />
+      <CardList cards={cards} deleteCard={deleteCard} />
+    </div>
+  );
+}
+
+export default App;
+    
